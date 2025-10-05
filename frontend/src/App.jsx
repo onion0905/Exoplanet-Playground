506cac44f390c04bcb178dc262c9ee61aba9ceb7
rainple0130 <rain0130@Rain-LAPTOP.>
2025-10-05 10:46:34 +0800

    Adjust route & new page import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ThemeProvider from "./components/ThemeProvider";
 import HomePage from "./pages/home/HomePage";
-import LearnPage from "./pages/learn/LearnPage";
+import LearnExoPage from "./pages/learn/LearnExoPage";
+import LearnMLPage from "./pages/learn/LearnMLPage";
 import SelectPage from "./pages/select/SelectPage";
+import PretrainedPage from "./pages/select/PretrainedPage";
 import TrainingPage from "./pages/training/TrainingPage";
-import PredictPage from "./pages/predict/PredictPage";
+import PredictPage from "./pages/predict(dropped)/PredictPage";
 import CustomResultPage from "./pages/result/CustomResultPage";
+import PretrainedResultPage from "./pages/result/PretrainedResultPage";
 
 function App() {
   return (
@@ -14,11 +17,14 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/learn" element={<LearnPage />} />
+          <Route path="/learn_exo" element={<LearnExoPage />} />
+          <Route path="/learn_ml" element={<LearnMLPage />} />
           <Route path="/select" element={<SelectPage />} />
+          <Route path="/pretrained" element={<PretrainedPage />} />
           <Route path="/training" element={<TrainingPage />} />
           <Route path="/predict" element={<PredictPage />} />
           <Route path="/custom_result" element={<CustomResultPage />} />
+          <Route path="/pretrained_result" element={<PretrainedResultPage />} />
         </Routes>
       </Router>
     </ThemeProvider>
